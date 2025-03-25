dc4128a7ae4f509aa229949cab133ba83919bf7c
hesoyam <mr.filonets-anton@yandex.ru>
2025-03-25 23:50:29 +0300

    /memory/hugearray: done

--- memory/hugearray/handler.c
+++ memory/hugearray/handler.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <bits/types/siginfo_t.h>
+#include <sys/mman.h>
 
 extern size_t PAGE_SIZE;
 extern double* SQRTS;
@@ -6,5 +9,14 @@ extern double* SQRTS;
 void CalculateSqrts(double* sqrt_pos, int start, int n);
 
 void HandleSigsegv(int sig, siginfo_t* siginfo, void* ctx) {
-    // Your code here
+    static void* ptr = NULL;
+
+    if (ptr) {
+        munmap(ptr, PAGE_SIZE);
+    }
+
+    double* pg_start = (double*) ((size_t) siginfo->si_addr & ~(PAGE_SIZE - 1));
+    ptr = mmap(pg_start, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, -1, 0);
+
+    CalculateSqrts(ptr, (double*) ptr - SQRTS, PAGE_SIZE / sizeof(double));
 }
