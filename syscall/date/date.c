06ab43f4a70d099cffbe29daa6d16d902688ead9
hesoyam <mr.filonets-anton@yandex.ru>
2025-04-11 01:06:35 +0300

    /syscall/date: mb done, but do not wont to try it local

--- syscall/date/date.c
+++ syscall/date/date.c
@@ -3,6 +3,15 @@
 #include "date.h"
 
 int main(int argc, char *argv[]) {
-    // Your code here
+    struct rtcdate d;
+    char buf[64];
+
+    if(date(&d) < 0) {
+        exit();
+    }
+
+    printf(1, "%d-%02d-%02dT%02d:%02d:%02d\n",
+           d.year, d.month, d.day, d.hour, d.minute, d.second);
+
     exit();
 }
