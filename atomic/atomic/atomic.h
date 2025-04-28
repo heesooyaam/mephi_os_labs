dff5ae4400e92296e9863bad896f798bb8e5763e
hesoyam <mr.filonets-anton@yandex.ru>
2025-04-28 22:28:16 +0300

    /atomic/atomic: done

--- atomic/atomic/atomic.h
+++ atomic/atomic/atomic.h
@@ -3,17 +3,50 @@
 #include <stdint.h>
 
 inline void AtomicAdd(int64_t* atomic, int64_t value) {
-    // Your code here
+    __asm__ volatile(
+        "lock addq %1, %0"
+        : "+m" (*atomic)
+        : "ir" (value)
+        : "memory", "cc"
+    );
 }
 
 inline void AtomicSub(int64_t* atomic, int64_t value) {
-    // Your code here
+    __asm__ volatile(
+        "lock subq %1, %0"
+        : "+m" (*atomic)
+        : "ir" (value)
+        : "memory", "cc"
+    );
 }
 
 inline int64_t AtomicXchg(int64_t* atomic, int64_t value) {
-    // Your code here
+    __asm__ volatile(
+        "lock xchgq %0, %1"
+        : "+r" (value), "+m" (*atomic)
+        :
+        : "memory", "cc"
+    );
+    return value;
 }
 
 inline int64_t AtomicCas(int64_t* atomic, int64_t* expected, int64_t value) {
-    // Your code here
+    int64_t expected_val = *expected;
+    unsigned char ok;
+
+    __asm__ volatile(
+        "lock cmpxchgq %3, %1\n\t"
+        "sete %b0"
+        : "=q" (ok),
+          "+m" (*atomic),
+          "+a" (expected_val)
+        : "r"  (value)
+        : "memory", "cc"
+    );
+
+    if (!ok) {
+        *expected = expected_val;
+    }
+
+    return ok;
 }
