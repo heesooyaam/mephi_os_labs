59d70c4bd1f610f8139982e6635226b19ebb7ac4
hesoyam <mr.filonets-anton@yandex.ru>
2025-04-29 02:06:46 +0300

    /atomic/seqlock: done

---
+++ atomic/seqlock/atomic.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <stdint.h>
+
+inline int64_t AtomicLoad(int64_t* atomic) {
+    int64_t ret;
+    asm volatile (
+        "movq %1, %0"
+        : "=r" (ret)
+        : "m" (*atomic)
+        : "memory"
+    );
+
+    return ret;
+}
+
+inline void AtomicAdd(int64_t* atomic, int64_t value) {
+    asm volatile(
+        "lock addq %1, %0"
+        : "+m" (*atomic)
+        : "ir" (value)
+        : "memory", "cc"
+    );
+}
+
+inline void AtomicSub(int64_t* atomic, int64_t value) {
+    asm volatile(
+        "lock subq %1, %0"
+        : "+m" (*atomic)
+        : "ir" (value)
+        : "memory", "cc"
+    );
+}
+
+inline int64_t AtomicXchg(int64_t* atomic, int64_t value) {
+    asm volatile(
+        "lock xchgq %0, %1"
+        : "+r" (value), "+m" (*atomic)
+        :
+        : "memory", "cc"
+    );
+    return value;
+}
+
+inline int64_t AtomicCas(int64_t* atomic, int64_t* expected, int64_t value) {
+    int64_t expected_val = *expected;
+    unsigned char ok;
+
+    asm volatile(
+        "lock cmpxchgq %3, %1\n\t"
+        "sete %b0"
+        : "=q" (ok),
+          "+m" (*atomic),
+          "+a" (expected_val)
+        : "r"  (value)
+        : "memory", "cc"
+    );
+
+    if (!ok) {
+        *expected = expected_val;
+    }
+
+    return ok;
+}
--- atomic/seqlock/seqlock.h
+++ atomic/seqlock/seqlock.h
@@ -1,21 +1,39 @@
 #pragma once
 
 #include <stdint.h>
+#include "spinlock.h"
+#include "atomic.h"
 
 struct SeqLock {
+    struct SpinLock lock;
+    int64_t atomic_seq;
 };
 
-inline void SeqLock_Init(struct SeqLock* lock) {
+inline void SeqLock_Init(struct SeqLock* sl) {
+    SpinLock_Init(&sl->lock);
+    sl->atomic_seq = 0;
 }
 
-inline int64_t SeqLock_ReadLock(struct SeqLock* lock) {
+inline int64_t SeqLock_ReadLock(struct SeqLock* sl) {
+    return AtomicLoad(&sl->atomic_seq);
 }
 
-inline int SeqLock_ReadUnlock(struct SeqLock* lock, int64_t value) {
+inline int SeqLock_ReadUnlock(struct SeqLock* sl, int64_t start) {
+    __sync_synchronize();
+    int64_t end = AtomicLoad(&sl->atomic_seq);
+    return (end == start) && ((start & 1) == 0);
 }
 
-inline void SeqLock_WriteLock(struct SeqLock* lock) {
+inline void SeqLock_WriteLock(struct SeqLock* sl) {
+    SpinLock_Lock(&sl->lock);
+    __sync_synchronize();
+    AtomicAdd(&sl->atomic_seq, 1);
+    __sync_synchronize();
 }
 
-inline void SeqLock_WriteUnlock(struct SeqLock* lock) {
+inline void SeqLock_WriteUnlock(struct SeqLock* sl) {
+    __sync_synchronize();
+    AtomicAdd(&sl->atomic_seq, 1);
+    __sync_synchronize();
+    SpinLock_Unlock(&sl->lock);
 }
