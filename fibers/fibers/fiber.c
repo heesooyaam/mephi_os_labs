bfbd7ccadd5e5c6d39b35e570d8b71f5f0eae1ea
hesoyam <mr.filonets-anton@yandex.ru>
2025-05-09 02:50:34 +0300

    /fibers/fibers: done

--- fibers/fibers/fiber.c
+++ fibers/fibers/fiber.c
@@ -1,3 +1,113 @@
 #include "fiber.h"
 
-// Your code here
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define FIBER_STACK_SIZE (64 * 1024)
+
+struct Fiber {
+    uint64_t rsp;
+    uint64_t rbx;
+    uint64_t rbp;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+
+    struct Fiber* next;
+    void (*f)(void*);
+    void*    args;
+    uint8_t* stack;
+    int      finished;
+};
+
+extern void SwitchFiberContext(struct Fiber* from, struct Fiber* to);
+static struct Fiber* CurrentFiber = NULL;
+
+static void InitMainFiber() {
+    if (CurrentFiber) {
+        return;
+    }
+
+    static struct Fiber Main = {0};
+    Main.next = &Main;
+    CurrentFiber = &Main;
+}
+
+static void FiberTrampoline() {
+    struct Fiber* self = CurrentFiber;
+    CurrentFiber->f(CurrentFiber->args);
+    CurrentFiber->finished = 1;
+    FiberYield();
+
+    // should never reach
+    assert(0);
+}
+
+static uint64_t PrepareStack(uint8_t* stack_top) {
+    *(uint64_t*)(stack_top - 8) = (uintptr_t) FiberTrampoline;
+    return (uint64_t) (stack_top - 8);
+}
+
+void FiberSpawn(void (*f)(void*), void* args) {
+    if (!f) {
+        return;
+    }
+
+    InitMainFiber();
+
+    struct Fiber* fiber = malloc(sizeof(struct Fiber));
+    fiber->f = f;
+    fiber->args = args;
+    fiber->finished = 0;
+    fiber->stack = malloc(FIBER_STACK_SIZE);
+    fiber->rsp = PrepareStack(fiber->stack + FIBER_STACK_SIZE);
+    fiber->next = CurrentFiber;
+
+    struct Fiber* tail = CurrentFiber;
+    while (tail->next != CurrentFiber) {
+        tail = tail->next;
+    }
+    tail->next = fiber;
+}
+
+static void FreeFinishedFibers() {
+    struct Fiber* prev = CurrentFiber;
+    struct Fiber* cur = prev->next;
+
+    while (cur != CurrentFiber) {
+        if (cur->finished) {
+            prev->next = cur->next;
+            free(cur->stack);
+            free(cur);
+
+            cur = prev->next;
+        } else {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+}
+
+void FiberYield() {
+    InitMainFiber();
+    FreeFinishedFibers();
+
+    struct Fiber* from = CurrentFiber;
+    struct Fiber* to = CurrentFiber->next;
+
+    if (to == from) {
+        return;
+    }
+
+    CurrentFiber = to;
+    SwitchFiberContext(from, to);
+}
+
+int FiberTryJoin() {
+    InitMainFiber();
+    FreeFinishedFibers();
+    return CurrentFiber->next == CurrentFiber;
+}
