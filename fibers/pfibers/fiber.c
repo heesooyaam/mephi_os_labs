0b5d68e9c66ad6ec46fb3b609a4c7eab766e8e37
hesoyam <mr.filonets-anton@yandex.ru>
2025-05-12 20:48:35 +0300

    ?

--- fibers/pfibers/fiber.c
+++ fibers/pfibers/fiber.c
@@ -126,7 +126,6 @@ void fiber_sched(int signum, siginfo_t *si, void *ucontext) {
         return;
     }
 
-    FiberYield();
 
     ucontext_t *uc = (ucontext_t *)ucontext;
 
@@ -135,6 +134,7 @@ void fiber_sched(int signum, siginfo_t *si, void *ucontext) {
     uint64_t *rsp = (uint64_t *)CurrentFiber->context.rsp - 1;
     *rsp = CurrentFiber->context.rip;
     CurrentFiber->context.rsp = (size_t) rsp;
+    FiberYield();
 
     // меняем контекст
     CurrentFiber = CurrentFiber->next;
